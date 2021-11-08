acc52c45dcedf2ffff845e2bed00671d03d18496
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-11-07 20:44:25 -0600

    Added functions withAuth and format_time in util folder

---
+++ utils/helpers.js
@@ -0,0 +1,13 @@
+module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
+  // The custom helper 'format_date' takes in a timestamp
+  format_date: (date) => {
+    // Using JavaScript Date methods, we get and format the month, date, and year
+    // We need to add one to the month since it is returned as a zero-based value
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+};
