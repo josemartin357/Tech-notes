af05accc8e772e15430e3591f8801365a9fc589b
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-11-10 01:09:52 -0600

    Added croute to post a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,21 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+// POST route for comment
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      comment: req.body.comment,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
 
 // NOTES FOR ROUTES
