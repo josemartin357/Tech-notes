5c369d7cd5b780bdf018a95ca9b4348a4f6ea8b5
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-11-10 01:01:59 -0600

    Added routes to create post, update post, delete post and get post by id

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+module.exports = router;
+
+// NOTES FOR ROUTES
+//  **** 1 *****
+
+// WHEN I enter a comment and click on the submit button while signed in
+// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
+// => POST ROUTE AT "/" (withAuth)
+// COMMENTROUTES
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,40 +1,131 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
-const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
-const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
+
+// CREATE one post
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      date_created: req.body.date_created,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// GET one post
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findOne(req.params.id, {
+      attributes: ["id", "title", "content", "date_created"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "date_created",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with that id!" });
+      return;
+    }
+    const post = postData.get({ plain: true });
+
+    res.render("singlePost", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// UPDATE one post
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE one post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
 
 // ROUTES NOTES
 
-//  **** 1 *****
+//  **** 1 ***** DONE
 
 // WHEN I click on an existing blog post
 // THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
 // => GET ROUTE "/:ID"
 // POSTROUTES
 
-// ***** 2 *****
-
-// WHEN I enter a comment and click on the submit button while signed in
-// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
-// => POST ROUTE AT "/" (withAuth)
-// POSTROUTES
-
-// ***** 3 ******
+// ***** 2 ****** DONE
 // WHEN I click on the button to add a new blog post
 // THEN I am prompted to enter both a title and contents for my blog post
-// GOES WITH BELOW??????
-
-// ***** 4 ******
 // WHEN I click on the button to create a new blog post
 // THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
 // => POST ROUTE AT "/", withAuth
 // POSTROUTES
 
-// ***** 5 ******
+// ***** 3 ****** DONE
 // WHEN I click on one of my existing posts in the dashboard
 // THEN I am able to delete or update my post and taken back to an updated dashboard
 // => PUT ROUTE AT ':ID'
